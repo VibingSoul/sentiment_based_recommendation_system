46a9d38709e42129b79b725d0c47122f1b958724
Hiranmai <heeru.m@gmail.com>
2025-02-18 01:44:39 +0530

    updated requirements files

--- app.py
+++ app.py
@@ -6,26 +6,32 @@ app = Flask('__name__')
 valid_userid = ['00sab00','1234','zippy','zburt5','joshua','dorothy w','rebecca','walker557','samantha','raeanne','kimmie','cassie','moore222']
 @app.route('/')
 def view():
-    return render_template('index.html')
+    return render_template('index.html', text=None)
 
 @app.route('/recommend',methods=['POST'])
 def recommend_top5():
-    print(request.method)
-    user_name = request.form['User Name']
-    print('User name=',user_name)
-    
+    user_name = request.form.get("username")
+    other_username = request.form.get("other_username")
+
+    # Use manually entered name if "Other" was selected
+    if user_name == "Other" and other_username:
+        user_name = other_username
+    print('user_name=',user_name)    
     if  user_name in valid_userid and request.method == 'POST':
             top20_products = model.recommend_products(user_name)
-            print(top20_products.head())
             get_top5 = model.top5_products(top20_products)
-            #return render_template('index.html',tables=[get_top5.to_html(classes='data',header=False,index=False)],text='Recommended products')
-            return render_template('index.html',column_names=get_top5.columns.values, row_data=list(get_top5.values.tolist()), zip=zip,text='Recommended products')
+            return render_template('index.html',
+                                   column_names=get_top5.columns.values, 
+                                   row_data=list(get_top5.values.tolist()), 
+                                   zip=zip,
+                                   text=f'Recommended products for {user_name}'
+                                   )
     elif not user_name in  valid_userid:
-        return render_template('index.html',text='No Recommendation found for the user')
+        return render_template('index.html',text=f'No Recommendation found for the user {user_name}')
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.debug=False
+    # app.debug=True
 
-    app.run()
+    app.run(debug=True)
